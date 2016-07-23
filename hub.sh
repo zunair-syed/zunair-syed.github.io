5e1ab4a637dc9294e2fa41893dc87e6b6dfc7fbb
Zunair <zunair_syed@hotmail.com>
2016-07-23 02:27:58 -0400

    automatic deployment commit

--- hub.sh
+++ hub.sh
@@ -2,5 +2,5 @@
 cd C:\Users\zunair\Documents\Zunair\Waterloo CE\Side Projects\Personal Website 2.0\repo\zunair-syed.github.io
 git status
 git add .
-git commit -m "made changes"
+git commit -m "automatic deployment commit"
 git push
